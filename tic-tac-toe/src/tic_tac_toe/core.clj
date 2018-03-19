5e58ea1c02a9117e4f89e3a781ac833a47c00a76
Emily A Grasmeder <egrasmed@thoughtworks.com>
2018-03-19 19:19:32 +0100

    Use map instead of array for board, check for wins

--- tic-tac-toe/src/tic_tac_toe/core.clj
+++ tic-tac-toe/src/tic_tac_toe/core.clj
@@ -1,9 +1,69 @@
 (ns tic-tac-toe.core
   (:gen-class))
 
-(defn base-board []
-  [[nil nil nil]
-   [nil nil nil]
-   [nil nil nil]])
+(def upper-left :00)
+(def upper-center :10)
+(def upper-right :20)
+(def center-left :01)
+(def center-center :11)
+(def center-right :21)
+(def bottom-left :02)
+(def bottom-center :12)
+(def bottom-right :22)
 
-(defn make-move [old marker "X" 2 2])
+(def base-board {upper-left nil upper-center nil upper-right nil
+                 center-left nil center-center nil center-right nil
+                 bottom-left nil bottom-center nil bottom-right nil})
+
+(defn make-move [old-matrix marker x y]
+  (assoc old-matrix (keyword (reduce str [x y])) marker))
+
+(defn get-moves-in-row [r]
+  (for [i r]
+    (filter #(not (nil? %))
+            (map (fn [i] (if (nil? (nth r i))
+                           i
+                           nil))
+                 r))))
+
+(defn get-non-nil-kvp [k v]
+ 
+ )
+
+(defn get-moves [board]
+  (keys (into {} (filter (comp nil? val) board))))
+
+(defn has-diagonal-winner? [board]
+  (or (= (upper-left board)
+         (center-center board)
+         (bottom-right board))
+      (= (upper-right board)
+         (center-center board)
+         (bottom-left board))))
+
+(defn has-horizontal-winner? [board]
+  (or (= (upper-left board)
+         (upper-center board)
+         (upper-right board))
+      (= (center-left board)
+         (center-center board)
+         (center-right board))
+      (= (bottom-left board)
+         (bottom-right board)
+         (bottom-right board))))
+
+(defn has-vertical-winner? [board]
+  (or (= (upper-left board)
+         (center-left board)
+         (bottom-left board))
+      (= (upper-center board)
+         (center-center board)
+         (bottom-center board))
+      (= (upper-right board)
+         (center-right board)
+         (bottom-right board))))
+
+(defn has-winner? [board]
+  (or (has-diagonal-winner? board)
+      (has-vertical-winner? board)
+      (has-horizontal-winner? board)))
--- tic-tac-toe/test/tic_tac_toe/core_test.clj
+++ tic-tac-toe/test/tic_tac_toe/core_test.clj
@@ -1,19 +1,63 @@
 (ns tic-tac-toe.core-test
-  (:require [clojure.test :refer :all]
-            [tic-tac-toe.core :as ttt]))
+ (:require [clojure.test :refer :all]
+  [tic-tac-toe.core :as ttt]))
 
 (deftest base-board-test
-  (testing "should start with a 3x3 board of 9 nil spaces"
-    (let [expected-board [[nil nil nil]
-                          [nil nil nil]
-                          [nil nil nil]]]
-      (is (= expected-board (ttt/base-board))))))
+ (testing "should start with a 3x3 board of 9 nil spaces"
+  (let [expected-board {:00 nil :10 nil :20 nil
+                        :01 nil :11 nil :21 nil
+                        :02 nil :12 nil :22 nil}]
+   (is (= expected-board ttt/base-board)))))
 
 (deftest make-move-test
-  (testing "should return new board with symbol at coordinates"
-    (let [initial-board (ttt/base-board)
-          expected-new-board [[nil nil nil]
-                              [nil nil nil]
-                              [nil nil "O"]]]
-      (is (= expected-new-board (ttt/make-move (ttt/base-board) "O" 2 2))))))
+ (testing "should change space at coordinates to specified value"
+  (is (= "O" (:22  (ttt/make-move ttt/base-board "O" 2 2))))
+  (is (= "X" (:11  (ttt/make-move ttt/base-board "X" 1 1))))
+  (is (= "hello world" (:00  (ttt/make-move ttt/base-board "hello world" 0 0)))))
+
+ (testing "should update board without changing existing moves"
+  (let [initial-board (assoc ttt/base-board :00 "X")
+        changed-board (ttt/make-move initial-board "O" 2 2)]
+   
+   (is (= "X" (:00 changed-board)))
+   (is (= "O" (:22 changed-board))))))
+
+(deftest get-moves-test  
+  (testing "should return coordinates of available moves"
+    (let [available-moves (keys ttt/base-board)]
+      (is (= available-moves (ttt/get-moves ttt/base-board))))))
+
+(deftest has-diagonal-winner?-test
+  (testing "should return true if one player has on a diagonal"
+    (let [board-with-descending-line-winner (assoc ttt/base-board :00 "X" :11 "X" :22 "X")
+          board-with-ascending-line-winner (assoc ttt/base-board :02 "X" :11 "X" :20 "X")]
+      (is (true? (ttt/has-diagonal-winner? board-with-descending-line-winner)))
+      (is (true? (ttt/has-diagonal-winner? board-with-ascending-line-winner))))))
+
+(deftest has-horizontal-winner?-test
+  (testing "should return true if one player has on a diagonal"
+    (let [board1 (assoc ttt/base-board :00 "X" :10 "X" :20 "X")
+          board2 (assoc ttt/base-board :01 "X" :11 "X" :21 "X")
+          board3 (assoc ttt/base-board :02 "X" :12 "X" :22 "X")]
+      (is (true? (ttt/has-horizontal-winner? board1)))
+      (is (true? (ttt/has-horizontal-winner? board2)))
+      (is (true? (ttt/has-horizontal-winner? board3))))))
+
+(deftest has-vertical-winner?-test
+  (testing "should return true if one player has on a diagonal"
+    (let [board1 (assoc ttt/base-board
+                        :00 "X"
+                        :01 "X"
+                        :02 "X")
+          board2 (assoc ttt/base-board
+                        :10 "X"
+                        :11 "X"
+                        :21 "X")
+          board3 (assoc ttt/base-board
+                        :20 "X"
+                        :21 "X"
+                        :22 "X")]
+      (is (true? (ttt/has-vertical-winner? board1)))
+      (is (true? (ttt/has-vertical-winner? board2)))
+      (is (true? (ttt/has-vertical-winner? board3))))))
 
